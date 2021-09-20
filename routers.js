8074f932523421b89673db528f3fe9c4a59f8477
Wallyson Pablo <wallysonpabloo@gmail.com>
2021-09-19 21:53:36 -0300

    ajusts

--- routers.js
+++ routers.js
@@ -8,5 +8,6 @@ route.get('/', homeController.index);
 
 // login routers
 route.get('/login/index', loginController.index);
+route.post('/login/register', loginController.register);
 
 module.exports = route;
