323c36ae4a27343909831cf4af74d1b19fd9ef65
Wallyson Pablo <wallysonpabloo@gmail.com>
2021-09-21 19:17:05 -0300

    contato cadastro start

---
+++ src/controllers/contatoController.js
@@ -0,0 +1,33 @@
+const Contato = require('../models/ContatoModel');
+
+exports.index = (req, res) => {
+    res.render('contato');
+};
+
+exports.register = async (req, res) => {
+    try {
+        const contato = new Contato(req.body);
+        await contato.register();
+    
+        if(contato.errors.length > 0) {
+            req.flash('errors', contato.errors);
+            req.session.save(() => res.redirect('/contato/index'));
+            return;
+        }
+    
+        req.flash('success', 'Contato registrado com sucesso');
+        req.session.save(() => res.redirect(`/contato/index/${contato.contato._id}`));
+        return;
+    } catch(e) {
+        return res.render('404');
+    }
+};
+
+exports.editIndex = async (req, res) => {
+    if(!req.params.id) return res.render('404');
+
+    const contato = await Contato.buscaPorId(req.params.id);
+    if(!contato) return res.render('404');
+
+    res.render('contato', { contato });
+};
