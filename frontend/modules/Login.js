6f0c15a49c0f765c173cc1889505b7d1ac11f73d
wpaq <wallysonpabloo@gmail.com>
2021-09-25 22:45:42 -0300

    server configs start

--- frontend/modules/Login.js
+++ frontend/modules/Login.js
@@ -1,10 +1,16 @@
+import validator from 'validator';
+import flash from 'connect-flash';
+
 export default class Login {
+    errors = []
+
     constructor(formClass) {
         this.form = document.querySelector(formClass);
     }
 
     init() {
-
+        this.events();
+        alert('init')
     }
 
     events() {
@@ -19,5 +25,24 @@ export default class Login {
         const el = e.target;
         const emailInput = el.querySelector('input[name="email"]');
         const passwordInput = el.querySelector('input[name="password"]');
+        let error = false;
+        // Validação
+        // O e-mail precisa ser válido
+        if(!validator.isEmail(emailInput.value)) {
+            this.errors.push('E-mail inválido');
+        }
+
+        // A senha precisa ter entre 3 e 50 
+        if(passwordInput.value.length < 3 || passwordInput.value.length >= 50) {
+            this.errors.push('A senha precisa ter entre 3 e 50 caracteres');
+        }     
+
+        if(this.errors.length > 0) {
+            // flash('errors', this.errors);
+            alert(this.errors)
+            return;
+        } else {
+            el.submit();
+        }
     }
 }
